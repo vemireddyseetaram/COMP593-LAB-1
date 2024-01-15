fe00738bb73add4167500bf80237be708318a31a
vemireddyseetaram <156461483+vemireddyseetaram@users.noreply.github.com>
2024-01-15 13:14:58 -0500

    Fixed empty file mistakes
    Copied files from Zen of Python

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("Although never is often better than *right* now")
+print("If the implementation is hard to explain, it's a bad idea.")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("In the face of ambiguity, refuse the temptation to guess.")
+print("Although that way may not be obvious at first unless you're Dutch.")
