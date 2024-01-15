549d51849f823dccd70040d6980546f1bf19d84e
vemireddyseetaram <156461483+vemireddyseetaram@users.noreply.github.com>
2024-01-15 13:46:32 -0500

    Modified both files in the repo done
    Deleted a print statement from Script1.py
    Added two print statements to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Files from Zen of python
+print("If the implementation is hard to explain, it's a bad idea.")
 print("In the face of ambiguity, refuse the temptation to guess.")
 print("Although that way may not be obvious at first unless you're Dutch.")
